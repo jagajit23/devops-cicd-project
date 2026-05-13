91bd31ab58bac65808933063fa16f0d6e7e1300a
jagajit23 <jagajit203@gmail.com>
2026-05-13 08:35:16 +0000

    Updated Flask message

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "DevOps CI/CD Pipeline Working Successfully!"
+    return "Jenkins Auto Deployment Working Successfully!"
 
 app.run(host='0.0.0.0', port=5000)
