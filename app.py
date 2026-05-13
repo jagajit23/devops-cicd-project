82bca1177ee2e09d3db67e354732b61f4f1281f4
jagajit23 <jagajit203@gmail.com>
2026-05-13 08:44:41 +0000

    Webhook auto deploy test

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Jenkins Auto Deployment Working Successfully!"
+    return "GitHub Webhook Auto Deployment Working!"
 
 app.run(host='0.0.0.0', port=5000)
